3b6d6442a0258e6a1fb259f4666dcb4a2798e1e2
Norman S <N.Sutatyo@gmail.com>
2020-03-13 12:21:17 +0100

    vorerst: dartPad Mode finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-//TODO: publish 4 Android
+/*
+Since dartpad doesn't support importing the http package, we use isntead of a real api call (line 55) a dummy-json-string (line 58).
+If you run this web-app on your local machine, uncomment lines 9, 55, 261-274, and comment out line 58.
+*/
+
+// import 'package:http/http.dart' as http; // uncomment this line, if running on a local machine with internet connection
 
 void main() {
   runApp(MyApp());
@@ -43,26 +47,22 @@ class _MyHomePageState extends State {
       targetAmountController.text = '';
       return;
     }
-    //case: baseCurrency == target Currency
     if (baseCurrencyName == targetCurrencyName) {
       targetAmountController.text = baseAmountController.text;
       return;
     }
-    String reqUrl =
-        'https://api.exchangeratesapi.io/latest?base=$baseCurrencyName';
-    Future<http.Response> resp = http.get(reqUrl);
-    resp.then((value) => processResp(value));
+
+    //makeApiCall(); //uncomment this line when running on a local machine with internet connection
+
+    //when running in dartpad:
+    calcAndShowResultInDartPad(jsonStrInDartPad);
   }
 
-  void processResp(http.Response resp) {
-    if (resp.statusCode != 200) {
-      showAlertDialog(context, 'Failed to load currency rates.');
-    } else {
-      Map<String, dynamic> respAsJson = jsonDecode(resp.body);
-      double rate = getRate(respAsJson);
-      double newTargetAmount = calcTargetAmount(rate);
-      setTargetAmount(newTargetAmount);
-    }
+  void calcAndShowResult(String jsonString) {
+    Map<String, dynamic> respAsJson = jsonDecode(jsonString);
+    double rate = getRate(respAsJson);
+    double newTargetAmount = calcTargetAmount(rate);
+    setTargetAmount(newTargetAmount);
   }
 
   void setTargetAmount(double newTargetAmount) {
@@ -235,6 +235,44 @@ class _MyHomePageState extends State {
     "PLN", //Zloty
     "HKD", //Hong Kong Dollar
   ];
+
+  final String jsonStrInDartPad =
+      '{"rates":{"CAD":1.5524,"HKD":8.7466,"ISK":148.64,"PHP":57.645,"DKK":7.4727,"HUF":338.37,"CZK":26.203,"AUD":1.7674,"RON":4.8213,"SEK":10.8945,"IDR":16434.0,"INR":83.468,"BRL":5.5081,"RUB":84.0284,"HRK":7.6,"JPY":116.84,"THB":35.586,"CHF":1.0549,"SGD":1.5779,"PLN":4.3599,"BGN":1.9558,"TRY":7.0361,"CNY":7.8877,"NOK":11.3682,"NZD":1.8173,"ZAR":18.4447,"USD":1.124,"MXN":24.8028,"ILS":4.0909,"GBP":0.88623,"KRW":1359.4,"MYR":4.7944},"base":"EUR","date":"2020-03-12"}';
+
+  void calcAndShowResultInDartPad(String jsonString) {
+    Map<String, dynamic> respAsJson = jsonDecode(jsonString);
+    double rate = getRateInDartPad(respAsJson);
+    double newTargetAmount = calcTargetAmount(rate);
+    setTargetAmount(newTargetAmount);
+  }
+
+  double getRateInDartPad(Map<String, dynamic> respAsJson) {
+    double result = -1;
+    Map<String, dynamic> rates = respAsJson['rates'];
+    if (baseCurrencyName != "EUR")
+      result = rates[targetCurrencyName] / rates[baseCurrencyName];
+    else
+      result = rates[targetCurrencyName];
+    return result;
+  }
+
+//Uncomment following lines if you run the app on a local machine with internet connection:
+
+//   void makeApiCall() {
+//     String reqUrl =
+//         'https://api.exchangeratesapi.io/latest?base=$baseCurrencyName';
+//     Future<http.Response> resp = http.get(reqUrl);
+//     resp.then((value) => processResp(value));
+//   }
+
+//     void processResp(http.Response resp) {
+//     if (resp.statusCode != 200) {
+//       showAlertDialog(context, 'Failed to load currency rates.');
+//     } else {
+//       calcAndShowResult(resp.body);
+//     }
+//   }
+
 }
 
 Future<void> showAlertDialog(BuildContext context, String msg) {
